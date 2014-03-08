ded23135cb39cd2c71bddeb7d17650f6cc8f1e53
PoweHHR <powerhhr@gmail.com>
2014-03-08 17:31:15 +0200

    Record Parser added
    Record Parser Added

---
+++ Sp_phase2/Sp_phase2/Records/LineParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Records
+{
+    class LineParser
+    {
+
+
+
+        //if null means parsing failed due to format exception
+        public Record TryParseLine(string recordStr)
+        {
+
+
+
+
+        }
+
+    }
+}
---
+++ Sp_phase2/Sp_phase2/Records/Record.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Records
+{
+    abstract class Record
+    {
+        byte  RecordType; //= 0 or 1
+        byte  count;    //because the maximum record count is limited to 79  characters
+        ushort address; 
+        byte checkSum;
+        byte[] data;
+    }
+
+    //class Record1 : Record
+    //{
+    //
+    //}
+    //class Record2 : Record
+    //{
+    //    byte[] data;
+    //}
+}
