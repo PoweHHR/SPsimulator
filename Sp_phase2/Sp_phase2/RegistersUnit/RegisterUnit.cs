e8c4d8f775393eeca16b142ae845ae5a65cf0d3b
PoweHHR <powerhhr@gmail.com>
2014-03-10 20:00:12 +0200

    Register Unit Added
    Register of 16 bit added

---
+++ Sp_phase2/Sp_phase2/RegistersUnit/RegisterUnit.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SP.Registers
+{
+    class Register
+    {
+        private int data;
+       
+        public byte lowByte{
+            get
+            {
+                return (byte)(data & 0x000000FF);
+            }
+            set
+            {
+                data =((data & 0x0000FF00) + value);
+            }
+        }
+        public byte highByte
+        {
+            get
+            {
+                return (byte) ( (data>>8) & 0x000000FF) ;
+            }
+            set
+            {
+
+                data = data & 0x000000FF + (value * 256);  
+            }
+        }
+        public ushort value{
+            get{
+                return (ushort)data;
+            }
+            set
+            {
+                data = value;
+            }
+            
+    
+        }
+
+       
+
+    }
+}
