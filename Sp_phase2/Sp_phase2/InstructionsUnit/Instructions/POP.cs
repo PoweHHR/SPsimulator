609d2147aa88710e0fd1b06251d60a2327b351e3
LAhmos <Lahmos4@gmail.com>
2014-03-19 14:05:43 +0200

    check N.Z in POP instruction

--- Sp_phase2/Sp_phase2/InstructionsUnit/Instructions/POP.cs
+++ Sp_phase2/Sp_phase2/InstructionsUnit/Instructions/POP.cs
@@ -36,6 +36,14 @@ namespace SP.InstructionsUnit.Instructions
                         {
                             regs[instr.rs].value = mem.getshortAt(regs[RegistersIndex.SP].value);
                             regs[RegistersIndex.SP].value = newSPAddress;
+                            if ((regs[instr.rs].value & 0x8000) == 0x8000)
+                                regs[RegistersIndex.CR][Register.N] = 1;
+                            else
+                                regs[RegistersIndex.CR][Register.N] = 0;
+                            if (regs[instr.rs].value == 0)
+                                regs[RegistersIndex.CR][Register.Z] = 1;
+                            else
+                                regs[RegistersIndex.CR][Register.Z] = 0;
                            
                         }
                     }
@@ -47,6 +55,14 @@ namespace SP.InstructionsUnit.Instructions
                         {
                             regs[instr.rs].lowByte = mem[regs[RegistersIndex.SP].value];
                             regs[RegistersIndex.SP].value = newSPAddress;
+                            if ((regs[instr.rs].lowByte & 0x0080) == 0x0080)
+                                regs[RegistersIndex.CR][Register.N] = 1;
+                            else
+                                regs[RegistersIndex.CR][Register.N] = 0;
+                            if (regs[instr.rs].lowByte == 0)
+                                regs[RegistersIndex.CR][Register.Z] = 1;
+                            else
+                                regs[RegistersIndex.CR][Register.Z] = 0;
 
                         }
                     }
